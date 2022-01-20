c47e0856af97e6cb0a8f6627fe7564741ceb6afd
softgiant-nICE <mailtrack00@gmail.com>
2022-01-19 22:41:41 -0800

    change port to 80

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 80;
 app.listen(port);
 
 console.log(`listening on ${port}`);
